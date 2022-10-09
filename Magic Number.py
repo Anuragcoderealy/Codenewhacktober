82b4cd398bcc3c3dae57d11e00f0304c2226f3dc
Bitan-Kundu <91739939+Bitan-Kundu@users.noreply.github.com>
2022-10-09 19:06:07 +0530

    Create Magic Number.py
    Added code for Magic Number

---
+++ Magic Number.py
@@ -0,0 +1,10 @@
+x = int(input("Enter the number: "))
+sum = 0
+for i in str(x):
+    sum = sum + int(i)
+number = sum
+
+if(number==1):
+    print("Magic Number")
+else:
+    print("Not a Magic Number")
