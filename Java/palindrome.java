8efb8152b6be04fe93be8b7a33cf24f83465e3d5
shivamdangi7 <shivamdangi0111@gmail.com>
2022-10-19 01:21:00 +0530

    Palindrom Program in java

---
+++ Java/palindrome.java
@@ -0,0 +1,17 @@
+// Java program to find palindrome number
+import java.util.*;
+public class palindrome {  
+   public static void main(String[] args) {  
+       Scanner sc = new Scanner(System.in);  
+       System.out.println("Enter the number: ");  
+       String reverse = ""; 
+       String num = sc.nextLine(); 
+       int length = num.length();   
+      for ( int i = length - 1; i >= 0; i-- )  
+         reverse = reverse + num.charAt(i);  
+      if (num.equals(reverse))  
+         System.out.println("The entered string " +num +" is a palindrome.");  
+      else  
+         System.out.println("The entered string " +num +"  isn't a palindrome.");     
+  }
+}
